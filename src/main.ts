cd98338f4b7965bd23fb495f1fccda27576f90ee
aniketdebnath <rkstaraniket34@gmail.com>
2025-04-28 14:09:31 +1000

    modules,controller,config added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  /**
+   * Swagger Configuration
+   */
+  const config = new DocumentBuilder()
+    .setTitle('Yeme API')
+    .setDescription('Yeme API Description')
+    .setVersion('1.0')
+    .addTag('yeme')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT ?? 3001);
 }
+
 bootstrap();
