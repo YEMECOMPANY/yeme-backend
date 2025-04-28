3373e641abaf699d1502cade4cf5e3ebffd88651
aniketdebnath <rkstaraniket34@gmail.com>
2025-04-28 15:48:50 +1000

    DB config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,10 +2,10 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('databaseConfig', () => ({
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'yeme_dev',
+  port: parseInt(process.env.DB_PORT ?? '1'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: process.env.DB_SYNC === 'true' ? true : false,
   autoLoadEntities: process.env.DB_AUTOLOAD === 'true' ? true : false,
 }));
